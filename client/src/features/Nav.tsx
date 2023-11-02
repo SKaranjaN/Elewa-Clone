80d40082830286518a3e1ae433773ca5789e9f08
stephen <skaranja654@gmail.com>
2023-11-02 21:17:18 +0300

    :memo: feat: linked the Nav component to the main.tsx

---
+++ client/src/features/Nav.tsx
@@ -0,0 +1,12 @@
+// This is the nav component, and regards to the MVVM architecture, it acts as the view model, whereas the json data is the model.
+
+
+function Nav() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Nav;
