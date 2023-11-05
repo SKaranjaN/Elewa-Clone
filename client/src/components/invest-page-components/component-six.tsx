1827e16b4777360169139729fd535d2a523695e9
stephen <skaranja654@gmail.com>
2023-11-05 17:20:44 +0300

    :memo: linked the sixth invest page component to the main tsx to render

---
+++ client/src/components/invest-page-components/component-six.tsx
@@ -0,0 +1,25 @@
+// This is thetSixth component of the invest page
+import React from "react";
+import "../../../src/style/invest-comp-6.css"; //this links the css file and the component
+import database from "../../../src/data/database.json";
+
+function ComponentSix() {
+    const componentSixData = database["invest-page"]["component-six"];
+  
+    return (
+      <div className="componentSix">
+        <div className="text">
+            <h1>{componentSixData.title}</h1>
+            <p>{componentSixData.p}</p>
+            <div>{componentSixData.button}</div>
+        </div>
+        <img
+          src={componentSixData.image[0]}
+          alt={componentSixData.image[1]}
+          className="component-image"
+        />
+      </div>
+    );
+  }
+  
+  export default ComponentSix;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import ComponentFive from './components/invest-page-components/component-five.tsx'
+import ComponentSix from './components/invest-page-components/component-six.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ComponentFive />
+    <ComponentSix />
   </React.StrictMode>,
 )
