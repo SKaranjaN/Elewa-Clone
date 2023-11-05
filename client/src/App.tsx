938bdade05680ec973f6edbff26bea8f29372672
stephen <skaranja654@gmail.com>
2023-11-05 22:56:37 +0300

    :memo: linked the about page component to app.tsx and used routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Invest from '../../client/src/pages/invest.tsx';
 import SocialImpact from '../../client/src/pages/social-impact';
+import About from './pages/about.tsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Invest />} />
         <Route path="/invest" element={<Invest />} />
         <Route path="/socialImpact" element={<SocialImpact />} />
+        <Route path="/aboutUs" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/about.tsx
@@ -0,0 +1,15 @@
+// this file will contain the components for the about page
+import React from "react";
+import Nav from "../features/Nav";
+import Footer from "../features/Footer";
+
+function About() {
+    return (
+        <div>
+            <Nav />
+            <Footer />
+        </div>
+    )
+}
+
+export default About;
