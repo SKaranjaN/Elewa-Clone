c912fbd1e1249ca77d19b38e450dbda032725943
stephen <skaranja654@gmail.com>
2023-11-05 14:35:37 +0300

    :memo: correctly fetched the data from my database to create my view-model

--- client/src/components/invest-page-components/component-one.tsx
+++ client/src/components/invest-page-components/component-one.tsx
@@ -1,11 +1,19 @@
+// This will be the first component on the invest page
 import React from "react";
+import database from "../../../src/data/database.json"; // this where we are linking the component to the database file
 
 function ComponentOne() {
+    // here we are getting the data from the JSON file
+    const investData = database["invest-page"]["component-one"];
+
     return (
-        <div>
-            <h1>Test</h1>
+        <div className="component-one">
+            <img src={investData.image[0]} alt={investData.image[1]} />
+            <h1 className="title">{investData.title[0]}</h1>
+            <h2 className="subtitle">{investData.title[1]}</h2>
+            <p className="tag">{investData.tag}</p>
         </div>
-    )
+    );
 }
 
 export default ComponentOne;
