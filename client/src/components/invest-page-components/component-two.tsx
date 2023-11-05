0a7edbc0fb53f728710424a00a716164d9e14066
stephen <skaranja654@gmail.com>
2023-11-05 15:21:32 +0300

    :memo: created the tsx elements and used the data from the json file

--- client/src/components/invest-page-components/component-two.tsx
+++ client/src/components/invest-page-components/component-two.tsx
@@ -1,15 +1,24 @@
 // This is the second component on the investment page
 
 import React from "react";
-import "../../../src/style/invest-comp-2.css"
+import database from "../../../src/data/database.json"; // This line imports the code from the json file
 
 function ComponentTwo() {
+    // Here is how we access the data for ComponentTwo from the JSON file
+    const investData = database["invest-page"]["component-two"];
 
     return (
-        <div className="component-one">
-            <h1>Test</h1>
+        <div className="component-two">
+            <h1>{investData.title}</h1>
+            <p>
+                {investData.p.map((paragraph, index) => (
+                    <span key={index}>{paragraph}<br /></span>
+                ))}
+            </p>
+            <img src={investData.image[0]} alt={investData.image[1]} />
         </div>
     );
 }
 
 export default ComponentTwo;
+
