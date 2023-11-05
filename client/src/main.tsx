bef00f50c46c03c666f5caaf4a2abd243699d813
stephen <skaranja654@gmail.com>
2023-11-05 18:31:18 +0300

    :memo: linked the seventh invest page component to the main tsx to render

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import ComponentSix from './components/invest-page-components/component-six.tsx'
+import ComponentSeven from './components/invest-page-components/component-seven.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ComponentSix />
+    <ComponentSeven />
   </React.StrictMode>,
 )
