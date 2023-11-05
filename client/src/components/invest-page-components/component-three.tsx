fe230ebcfebcef13ae7d004251bf6d6e13e0bb81
stephen <skaranja654@gmail.com>
2023-11-05 15:54:20 +0300

    :memo: fetched the data from the json file and created the div and p elements to properly render the data

--- client/src/components/invest-page-components/component-three.tsx
+++ client/src/components/invest-page-components/component-three.tsx
@@ -1,15 +1,26 @@
-// This is the second component on the investment page
-
+// This is the third component of the invest page
 import React from "react";
-import "../../../src/style/invest-comp-3.css"
+import "../../../src/style/invest-comp-3.css";
+import database from "../../../src/data/database.json"; //here is how we link the component to the json file
 
 function ComponentThree() {
+    // here is where we access the data from the json file
+  const componentThreeData = database["invest-page"]["component-three"];
 
-    return (
-        <div className="component-two">
-            <h1>Test</h1>
-        </div>
-    );
+  return (
+    <div className="component-three">
+      <h1>{componentThreeData.title}</h1>
+      <div className="cards-container">
+        {/* here we are iterating over the cards in order to render them all */}
+        {Object.entries(componentThreeData.cards).map(([, cardData], index) => (
+          <div className="card" key={index}>
+            <p className="number">{cardData.number}</p>
+            <p className="description">{cardData.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ComponentThree;
