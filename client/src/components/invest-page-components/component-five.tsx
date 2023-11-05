8f678e8a7e4e837541d23647e6817d87a29dc0d9
stephen <skaranja654@gmail.com>
2023-11-05 17:12:20 +0300

    :memo: changed the rendering order of the image and the text in order to make the text appear on the left of the image

--- client/src/components/invest-page-components/component-five.tsx
+++ client/src/components/invest-page-components/component-five.tsx
@@ -7,17 +7,17 @@ function ComponentFive() {
     const componentFiveData = database["invest-page"]["component-five"];
   
     return (
-      <div className="componenttFive">
-        <img
-          src={componentFiveData.image[0]}
-          alt={componentFiveData.image[1]}
-          className="component-image"
-        />
+      <div className="componentFive">
         <div className="text">
             <h1>{componentFiveData.title}</h1>
             <p>{componentFiveData.p1}</p>
             <p>{componentFiveData.p2}</p>
         </div>
+        <img
+          src={componentFiveData.image[0]}
+          alt={componentFiveData.image[1]}
+          className="component-image"
+        />
       </div>
     );
   }
