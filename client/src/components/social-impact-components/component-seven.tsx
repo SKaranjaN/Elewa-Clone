f1fbb7d788920d41597805a9b8be8b0a9ea80522
stephen <skaranja654@gmail.com>
2023-11-05 21:51:30 +0300

    :memo: accessed the images and the title from the json file and correctly rendered them

---
+++ client/src/components/social-impact-components/component-seven.tsx
@@ -0,0 +1,27 @@
+// this is the seventh component in the social impact page
+import React from 'react'; 
+import "../../../src/style/soc-imp-7.css"
+import database from "../../../src/data/database.json"; // this where we are linking the component to the database file
+
+function ComponentSeven() {
+    const componentSevenData = database['social-impact-page']['component-7']; //this allows us to access the data from the json file
+    const carouselImages = Object.values(componentSevenData.carousel);
+  
+    return (
+      <div className="component-Seven">
+        <h1 className="text">{componentSevenData.title}</h1>
+        <div className="carousel-container">
+          {carouselImages.map((image, index) => (
+            <img
+              key={index}
+              src={image[0]}
+              alt={`Image ${index + 1}`}
+              className="carousel-image"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
+  export default ComponentSeven;
