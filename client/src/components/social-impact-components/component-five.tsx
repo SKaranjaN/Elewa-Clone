e4f2dd6cd5b7eb8e49458752a61dbbae13c42316
stephen <skaranja654@gmail.com>
2023-11-05 21:13:20 +0300

    :art: reused styling from similar components

---
+++ client/src/components/social-impact-components/component-five.tsx
@@ -0,0 +1,24 @@
+// this is the Fiveth component in the social impact page 
+import React from "react";
+import "../../../src/style/soc-imp-4.css"
+import database from "../../../src/data/database.json"; // this where we are linking the component to the database file
+
+function ComponentFive() {
+    // here we are accessing the data from the JSON file
+    const componentFiveData = database["social-impact-page"]["component-5"];
+  
+    return (
+        <div className="component-Five">
+            <img
+                src={componentFiveData.image[0]}
+                className="component-image"
+            />
+          <div className="text">
+              <h1>{componentFiveData.title}</h1>
+              <p>{componentFiveData.p1}</p>
+          </div>
+        </div>
+      );
+    }
+  
+  export default ComponentFive;
