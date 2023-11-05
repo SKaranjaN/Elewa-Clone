587e2b7bcc0c461f531a7c72583a280c2ccfc19e
stephen <skaranja654@gmail.com>
2023-11-05 16:34:11 +0300

    :memo: linked the css file to the fourth component of the investment page

---
+++ client/src/components/invest-page-components/component-four.tsx
@@ -0,0 +1,14 @@
+// This is the fourth component of the invest page
+import React from "react";
+import "../../../src/style/invest-comp-4.css"; //this links the css file and the component
+
+function ComponentThree() {
+
+  return (
+    <div>
+        <h1>Test</h1>
+    </div>
+  );
+}
+
+export default ComponentThree;
