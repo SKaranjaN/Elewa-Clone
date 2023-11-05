ba4b751cc15b3b46e3b7d9ea347596ae8a8b9f33
stephen <skaranja654@gmail.com>
2023-11-05 16:39:50 +0300

    :memo: linked the component to the database json file

--- client/src/components/invest-page-components/component-four.tsx
+++ client/src/components/invest-page-components/component-four.tsx
@@ -1,14 +1,23 @@
 // This is the fourth component of the invest page
 import React from "react";
 import "../../../src/style/invest-comp-4.css"; //this links the css file and the component
+import database from "../../../src/data/database.json";
 
 function ComponentFour() {
-
-  return (
-    <div>
-        <h1>Test</h1>
-    </div>
-  );
-}
-
-export default ComponentFour;
+    const componentFourData = database["invest-page"]["component-four"];
+  
+    return (
+      <div className="component-four">
+        <img
+          src={componentFourData.image[0]}
+          alt={componentFourData.image[1]}
+          className="component-image"
+        />
+        <h1>{componentFourData.title}</h1>
+        <p>{componentFourData.p1}</p>
+        <p>{componentFourData.p2}</p>
+      </div>
+    );
+  }
+  
+  export default ComponentFour;
